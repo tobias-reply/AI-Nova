6f545f9624f5d41a9122a17e700059d4601a64b3
Tobias Kapp <t.kapp@reply.de>
2025-08-27 09:08:14 +0200

    test1

---
+++ dynamodb_test.py
@@ -0,0 +1,29 @@
+import boto3
+from typing import Dict, Any
+
+def write_to_dynamodb(table_name, item):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    
+    response = table.put_item(Item=item)
+    
+    delete_item_from_dynamodb(table_name, item)
+    
+    return response
+
+def delete_item_from_dynamodb(table_name, item):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    
+    key = {}
+    for key_name in ['id', 'pk', 'primary_key']:
+        if key_name in item:
+            key[key_name] = item[key_name]
+            break
+    
+    response = table.delete_item(Key=key)
+    return response
+
+def process_data(table_name, data_object):
+    result = write_to_dynamodb(table_name, data_object)
+    return result
